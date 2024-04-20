9cca1506eefe3f0107911cb08f6ac77bacf275bd
ishakash <isha0911@hotmail.com>
2024-04-20 13:12:11 +0200

    on_train_epoch_end fix

--- finetune.py
+++ finetune.py
@@ -81,7 +81,7 @@ class SimCLR_eval(pl.LightningModule):
 
        return loss
     
-    def training_epoch_end(self, outputs):
+    def on_train_epoch_end(self, outputs):
         avg_acc = self.accuracy.compute()
         avg_top5_acc = self.top5_accuracy.compute()
         self.epoch_accuracies.append(avg_acc.item())  # Store the average Top-1 accuracy of the epoch
