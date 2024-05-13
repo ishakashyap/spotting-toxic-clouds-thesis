41a6ce02f9a60218742832acd1540ad6bbd32527
ishakash <isha0911@hotmail.com>
2024-05-13 15:00:55 +0200

    Added mlp layer

--- finetune.py
+++ finetune.py
@@ -457,8 +457,6 @@ if __name__ == '__main__':
     #         new_key = key.replace('fc.bias', 'classifier.2.bias')
     #     adjusted_state_dict[new_key] = value
 
-    # state_dict = fix_state_dict(checkpoint['model_state_dict'])
-    print(checkpoint['model_state_dict'])
     # Initialize your model
     model = SimCLR_eval(hidden_dim=224, lr=1e-3, fine_tune=False, linear_eval=True)
 
