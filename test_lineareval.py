1ef4c85e0f56a3a48b6248968bb572c7ec2a0339
ishakash <isha0911@hotmail.com>
2024-05-25 09:50:37 +0200

    Added sampler

--- test_lineareval.py
+++ test_lineareval.py
@@ -8,7 +8,8 @@ import seaborn as sns
 import torch.optim as optim
 from torchvision.io import read_video
 from torchvision import transforms, models
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler
+from imblearn.over_sampling import RandomOverSampler
 from torchvision.transforms import functional as F
 from torchvision.models.video import r3d_18, R3D_18_Weights
 from sklearn.metrics import classification_report, f1_score, confusion_matrix
@@ -41,6 +42,9 @@ class VideoDataset(Dataset):
         # Load labels from JSON file
         self.labels = self._load_labels(labels_json_path)
         self.video_files = self.validate_videos_and_labels()
+
+        self.targets = [self.labels[video_file] for video_file in self.video_files]
+        self.targets = torch.tensor(self.targets)
 
     def _load_labels(self, labels_json_path):
         with open(labels_json_path, 'r') as f:
@@ -105,85 +109,180 @@ class VideoDataset(Dataset):
         video_tensor = torch.stack(transformed_frames)
         return video_tensor.permute(1, 0, 2, 3)
     
-def plot_confusion_matrix(y_true, y_pred, classes, title='Confusion matrix', filename='conf_matrix.png'):
+def get_oversampled_loader(dataset):
+    targets = dataset.targets.numpy()
+    class_sample_count = np.array([len(np.where(targets == t)[0]) for t in np.unique(targets)])
+    weight = 1. / class_sample_count
+    samples_weight = np.array([weight[t] for t in targets])
+
+    samples_weight = torch.from_numpy(samples_weight)
+    sampler = WeightedRandomSampler(samples_weight.type('torch.DoubleTensor'), len(samples_weight))
+
+    return sampler
+    
+def plot_confusion_matrix(y_true, y_pred, classes, title='Confusion matrix', cm_filename='conf_matrix.png', cr_filename='clf_report.txt'):
     cm = confusion_matrix(y_true, y_pred)
     plt.figure(figsize=(8, 6))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
     plt.xlabel('Predicted')
     plt.ylabel('True')
     plt.title(title)
-    plt.savefig(filename)
+    plt.savefig(cm_filename)
     plt.show()
     plt.close()
 
-def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
-    best_model_wts = model.state_dict()
-    best_acc = 0.0
+    report = classification_report(y_true, y_pred, target_names=classes)
+    with open(cr_filename, 'w') as f:
+        f.write(report)
 
+def train(train_loader, val_loader, model, optimizer, criterion, num_epochs):
     for epoch in range(num_epochs):
-        print(f'Epoch {epoch}/{num_epochs - 1}')
-        print('-' * 10)
+        model.train()
+        train_loss = 0.0
+        all_preds = []
+        all_labels = []
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
+        for views, labels in train_loader:
+            if views is None or labels is None:
+                continue
 
-            running_loss = 0.0
-            running_corrects = 0
-            all_preds = []
-            all_labels = []
+            views, labels = views.cuda(), labels.cuda()
+            optimizer.zero_grad()
+            outputs = model(views)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * views.size(0)
 
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.cuda()
-                labels = labels.cuda()
+            preds = torch.argmax(outputs, dim=1)
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
 
-                # Zero the parameter gradients
-                optimizer.zero_grad()
+        epoch_loss = train_loss / len(train_loader.dataset)
+        print(f'Epoch {epoch+1}/{num_epochs}, Training Loss: {epoch_loss:.4f}')
+        print('Training Classification Report:')
+        print(classification_report(all_labels, all_preds, target_names=['Class 0', 'Class 1']))
+        plot_confusion_matrix(all_labels, all_preds, classes=['Class 0', 'Class 1'], title=f'Training Confusion Matrix Epoch {epoch+1}', cm_filename=f'training_confusion_matrix_epoch_{epoch+1}.png', cr_filename=f'training_classification_report_epoch_{epoch+1}.txt')
 
-                # Forward
-                # Track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
+        model.eval()
+        val_loss = 0.0
+        all_preds = []
+        all_labels = []
 
-                    # Backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+        with torch.no_grad():
+            for views, labels in val_loader:
+                if views is None or labels is None:
+                    continue
 
-                # Statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                views, labels = views.cuda(), labels.cuda()
+                outputs = model(views)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item() * views.size(0)
+
+                preds = torch.argmax(outputs, dim=1)
                 all_preds.extend(preds.cpu().numpy())
                 all_labels.extend(labels.cpu().numpy())
 
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
-            epoch_f1 = f1_score(all_labels, all_preds, average='weighted')
+        epoch_loss = val_loss / len(val_loader.dataset)
+        print(f'Epoch {epoch+1}/{num_epochs}, Validation Loss: {epoch_loss:.4f}')
+        print('Validation Classification Report:')
+        print(classification_report(all_labels, all_preds, target_names=['Class 0', 'Class 1']))
+        plot_confusion_matrix(all_labels, all_preds, classes=['Class 0', 'Class 1'], title=f'Validation Confusion Matrix Epoch {epoch+1}', cm_filename=f'validation_confusion_matrix_epoch_{epoch+1}.png', cr_filename=f'validation_classification_report_epoch_{epoch+1}.txt')
 
-            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} F1: {epoch_f1:.4f}')
-            clf_report = classification_report(all_labels, all_preds, target_names=['Class 0', 'Class 1'])
-            print(f'{phase} classification Report: ', clf_report)
-            # print(classification_report(all_labels, all_preds, target_names=['Class 0', 'Class 1']))
-            plot_confusion_matrix(all_labels, all_preds, classes=['Class 0', 'Class 1'], title=f'{phase} Confusion Matrix', filename=f'./{phase}_conf.png')
+def test(test_loader, model, criterion):
+    model.eval()
+    test_loss = 0.0
+    all_preds = []
+    all_labels = []
 
-            with open(f'./{phase}_clf_report.txt', 'w') as f:
-                f.write(clf_report)
+    with torch.no_grad():
+        for views, labels in test_loader:
+            if views is None or labels is None:
+                continue
 
-            # Deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = model.state_dict()
+            views, labels = views.cuda(), labels.cuda()
+            outputs = model(views)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item() * views.size(0)
 
-    # Load best model weights
-    model.load_state_dict(best_model_wts)
+            preds = torch.argmax(outputs, dim=1)
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
 
-    return model
+    epoch_loss = test_loss / len(test_loader.dataset)
+    print(f'Test Loss: {epoch_loss:.4f}')
+    print('Test Classification Report:')
+    print(classification_report(all_labels, all_preds, target_names=['Class 0', 'Class 1']))
+    plot_confusion_matrix(all_labels, all_preds, classes=['Class 0', 'Class 1'], title='Test Confusion Matrix', cm_filename='test_confusion_matrix.png', cr_filename='test_classification_report.txt')
+
+# def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
+#     best_model_wts = model.state_dict()
+#     best_acc = 0.0
+
+#     for epoch in range(num_epochs):
+#         print(f'Epoch {epoch}/{num_epochs - 1}')
+#         print('-' * 10)
+
+#         # Each epoch has a training and validation phase
+#         for phase in ['train', 'val']:
+#             if phase == 'train':
+#                 model.train()  # Set model to training mode
+#             else:
+#                 model.eval()   # Set model to evaluate mode
+
+#             running_loss = 0.0
+#             running_corrects = 0
+#             all_preds = []
+#             all_labels = []
+
+#             # Iterate over data.
+#             for inputs, labels in dataloaders[phase]:
+#                 inputs = inputs.cuda()
+#                 labels = labels.cuda()
+
+#                 # Zero the parameter gradients
+#                 optimizer.zero_grad()
+
+#                 # Forward
+#                 # Track history if only in train
+#                 with torch.set_grad_enabled(phase == 'train'):
+#                     outputs = model(inputs)
+#                     _, preds = torch.max(outputs, 1)
+#                     loss = criterion(outputs, labels)
+
+#                     # Backward + optimize only if in training phase
+#                     if phase == 'train':
+#                         loss.backward()
+#                         optimizer.step()
+
+#                 # Statistics
+#                 running_loss += loss.item() * inputs.size(0)
+#                 running_corrects += torch.sum(preds == labels.data)
+#                 all_preds.extend(preds.cpu().numpy())
+#                 all_labels.extend(labels.cpu().numpy())
+
+#             epoch_loss = running_loss / len(dataloaders[phase].dataset)
+#             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+#             epoch_f1 = f1_score(all_labels, all_preds, average='weighted')
+
+#             print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} F1: {epoch_f1:.4f}')
+#             clf_report = classification_report(all_labels, all_preds, target_names=['Class 0', 'Class 1'])
+#             print(f'{phase} classification Report: ', clf_report)
+#             # print(classification_report(all_labels, all_preds, target_names=['Class 0', 'Class 1']))
+#             plot_confusion_matrix(all_labels, all_preds, classes=['Class 0', 'Class 1'], title=f'{phase} Confusion Matrix', filename=f'./{phase}_conf.png')
+
+#             with open(f'./{phase}_clf_report.txt', 'w') as f:
+#                 f.write(clf_report)
+
+#             # Deep copy the model
+#             if phase == 'val' and epoch_acc > best_acc:
+#                 best_acc = epoch_acc
+#                 best_model_wts = model.state_dict()
+
+#     # Load best model weights
+#     model.load_state_dict(best_model_wts)
+
+#     return model
 
 def test_model(model, dataloader, criterion):
     model.eval()
@@ -269,11 +368,14 @@ def main():
     val_dataset = VideoDataset(val_folder, val_label_folder, transform=train_transforms)
     test_dataset = VideoDataset(test_folder, test_label_folder, transform=train_transforms)
 
-    dataloaders = {
-        'train': DataLoader(train_dataset, batch_size=8, shuffle=False, num_workers=0, pin_memory=True),
-        'val': DataLoader(val_dataset, batch_size=8, shuffle=False, num_workers=0, pin_memory=True),
-        # 'test': DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=0, pin_memory=True)
-    }
+    train_sampler = get_oversampled_loader(train_dataset)
+    val_sampler = get_oversampled_loader(val_dataset)
+    test_sampler = get_oversampled_loader(test_dataset)
+
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=False, num_workers=0, pin_memory=True, sampler=train_sampler)
+    val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False, num_workers=0, pin_memory=True, sampler=val_sampler)
+    test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=0, pin_memory=True, sampler=test_sampler)
+    
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -296,20 +398,21 @@ def main():
 
     self_supervised_model = self_supervised_model.to(device)
 
-    train_labels = extract_labels_from_dataset(train_dataset)
-    class_weights = calculate_class_weights(train_labels)
-    criterion = nn.CrossEntropyLoss(weight=class_weights.cuda())
+    # train_labels = extract_labels_from_dataset(train_dataset)
+    # class_weights = calculate_class_weights(train_labels)
+    criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(self_supervised_model.fc.parameters(), lr=0.001, momentum=0.9)
 
     # if 'optimizer_state_dict' in checkpoint:
     #     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     # Train and evaluate the model
-    model = train_model(self_supervised_model, dataloaders, criterion, optimizer, num_epochs=2)
+    # model = train_model(self_supervised_model, dataloaders, criterion, optimizer, num_epochs=2)
     # test_model(model, dataloaders['test'], criterion)
-
+    train(train_loader=train_loader, val_loader=val_loader, model=self_supervised_model, optimizer=optimizer, criterion=criterion, num_epochs=3)
+    test(test_loader=test_loader, model=self_supervised_model, criterion=criterion)
     # Save the trained model
-    torch.save(model.state_dict(), 'linear_eval_model.pth')
+    torch.save(self_supervised_model.state_dict(), 'linear_eval_model.pth')
 
 if __name__ == "__main__":
     main()
