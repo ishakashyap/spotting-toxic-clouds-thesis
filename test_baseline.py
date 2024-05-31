4cc9d3c2266438c91a8c655c600318b899896bd0
ishakash <isha0911@hotmail.com>
2024-05-31 10:15:24 +0200

    Tweaked lr parameter

--- test_baseline.py
+++ test_baseline.py
@@ -323,7 +323,7 @@ def main():
     self_supervised_model = self_supervised_model.to(device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(self_supervised_model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(self_supervised_model.parameters(), lr=0.0001, momentum=0.9)
     scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
 
     # if 'optimizer_state_dict' in checkpoint:
