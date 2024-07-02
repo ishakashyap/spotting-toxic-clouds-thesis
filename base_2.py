aacdd5d81858e152466f90ce8193f6acf100af9e
ishakash <isha0911@hotmail.com>
2024-07-02 09:45:15 +0200

    Re-run with only cols

--- base_2.py
+++ base_2.py
@@ -299,14 +299,14 @@ def main():
     train_transforms = transforms.Compose([
         transforms.Resize(224), 
         # Blur
-        transforms.RandomApply([transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))], p=0.5),
+        # transforms.RandomApply([transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))], p=0.5),
         # Color
         transforms.RandomApply([transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)], p=0.8),
         transforms.RandomGrayscale(p=0.2),
         # Dimension
-        transforms.RandomHorizontalFlip(), 
-        transforms.RandomRotation(degrees=15),
-        transforms.CenterCrop(224),
+        # transforms.RandomHorizontalFlip(), 
+        # transforms.RandomRotation(degrees=15),
+        # transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
@@ -376,7 +376,7 @@ def main():
     #     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     # Train and evaluate the model
-    train(train_loader=train_loader, val_loader=val_loader, test_loader=test_loader, model=self_supervised_model, optimizer=optimizer, criterion=criterion, num_epochs=20, scheduler=scheduler) # , patience=5, min_delta=0.001
+    train(train_loader=train_loader, val_loader=val_loader, test_loader=test_loader, model=self_supervised_model, optimizer=optimizer, criterion=criterion, num_epochs=8, scheduler=scheduler) # , patience=5, min_delta=0.001
 
     # Save the trained model
     torch.save(self_supervised_model.state_dict(), 'baseline_model.pth')
